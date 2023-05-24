987b9c395e98e6ff18fc220b75059e73cc3c0584
SeungHyeok Choi <130980579+kopfstand1931@users.noreply.github.com>
2023-05-24 16:49:44 +0900

    feat: 통계 임시 구현 수정

--- 최승혁/cshHW3/StatisticsApplyInformation.cpp
+++ 최승혁/cshHW3/StatisticsApplyInformation.cpp
@@ -31,24 +31,24 @@ std::map<std::string, int> StatisticsApplyInformation::ShowStaticsApplyInformati
 
     // 회사 회원의 경우
     if(memberType.compare("1") != 0){
-        vector<*Recruitment> taskList = member->GetRecruitmentList();
+        vector<Recruitment*> taskList = member->GetRecruitmentList();
         
         // 초기화. 더 나은 방법이 떠오르시면 고쳐주세요 :)
         for (const auto& statisticInfo : taskList) {
-            std::string task = statisticInfo.GetTask();
+            std::string task = (*statisticInfo).GetTask();
             taskCountMap[task] = 0;
         }
 
         for (const auto& statisticInfo : taskList) {
-            std::string task = statisticInfo.GetTask();
-            taskCountMap[task] += statisticInfo.GetApplyNumber;
+            std::string task = (*statisticInfo).GetTask();
+            taskCountMap[task] += (*statisticInfo).GetApplyNumber();
         }
     }
     // 일반 회원의 경우
     else{
-        vector<*ApplyInfo> taskList = member->GetApplyList();
+        vector<ApplyInfo*> taskList = member->GetApplyList();
         for (const auto& statisticInfo : taskList) {
-            std::string task = statisticInfo.GetTask();
+            std::string task = (*statisticInfo).GetTask();
             taskCountMap[task]++;
         }
     }
